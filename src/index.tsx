af5a7e1f6a29ecbe9856e7abe1e5287ed41fc829
island-da <sknt0309@gmail.com>
2023-05-16 10:35:05 +0900

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Parent } from './components/UseCallbackSample';
+
+ReactDOM.render(
+  <Parent />,
+  document.getElementById('root')
+)
